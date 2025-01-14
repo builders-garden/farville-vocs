c2a5417bad7aada266a404cdd7429c7b0bdf2db7
lim0n3 <simonestaffa96@gmail.com>
2025-01-14 15:34:18 +0100

    update config

--- farville/vocs.config.ts
+++ farville/vocs.config.ts
@@ -1,30 +1,26 @@
-import { defineConfig } from 'vocs'
+import { defineConfig } from "vocs";
 
 export default defineConfig({
-  socials: [ 
-    { 
-      icon: 'warpcast', 
-      link: 'https://github.com/wevm/viem', 
-    }, 
-    { 
-      icon: 'x', 
-      link: 'https://twitter.com/wevm_dev', 
-    }, 
-  ], 
-  title: 'Farville',
-  //logoUrl: '/farville-logo.jpeg', 
+  socials: [
+    {
+      icon: "warpcast",
+      link: "https://warpcast.com/~/channel/farville",
+    },
+  ],
+  title: "Farville",
+  //logoUrl: '/farville-logo.jpeg',
   sidebar: [
     {
-      text: 'Getting Started',
-      link: '/getting-started',
+      text: "Getting Started",
+      link: "/getting-started",
     },
     {
-      text: 'Gameplay',
-      link: '/gameplay',
+      text: "Gameplay",
+      link: "/gameplay",
     },
   ],
-  topNav: [ 
-    { text: 'Learn More', link: '/getting-started' }, 
-    { text: 'Play', link: 'https://github.com/wevm/vocs/blob/main/src/CHANGELOG.md' }, 
+  topNav: [
+    { text: "Learn More", link: "/getting-started" },
+    { text: "Play", link: "https://warpcast.com/~/channel/farville" },
   ],
-})
+});
